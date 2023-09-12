5b6cbab8570225d85bff58dc655e1a9f25d93888
shahhuzaifa <143290124+shahhuzaifa@users.noreply.github.com>
2023-09-12 15:58:36 +0500

    Add files via upload

---
+++ page.js
@@ -0,0 +1,174 @@
+//answer2 
+var a = 100;
+var b= --a;
+console.log(b);
+
+//question 3
+
+var x = 50;
+var y = x++;
+console.log(y);
+
+//question 4
+var y = 50;
+var z = --y;
+console.log (z);
+
+//answer5 
+var num = 10; 
+var newNum = num--; 
+console.log (newNum);
+
+
+//answer6 
+var num1 = 5; 
+var num2 = num1++; 
+console.log (num2);
+
+
+//answer7
+var d= 104 ;
+var e= ++d ;
+//alert(e);
+
+                          //ch 7
+
+                          //answer1
+                          var mul = 2 + (2 * 6);
+                          console.log(mul);
+
+                          //answer 2
+                          var num1 = (2 + 2) * 6;
+                          console.log(num1);
+
+                          //answer 3
+                          var num2 = (2 + 2) * (4 + 2);
+                          console.log(num2);
+
+                          //answer 4
+                          var Num3 = ((2 + 2) * 4) + 2;
+                          console.log (Num3);
+
+                          //answer 5
+                          var cost = (2 + 2) * ( 4 + 10);
+                          console.log(cost);
+
+                          //answer6 
+                          var units  = (2 + 2 * 4 )+ 10;
+                          console.log(units);
+
+                          //answer 7
+
+
+
+                                          //ch 8 
+
+
+//answer 1 
+var con = "2" + "2";
+console.log(con);
+
+//answer 2
+var message = "Hello," + "Dolly";
+//alert('"' + message + '"');
+
+
+//answer 3
+//alert("33" + 3);
+
+//answer 4
+var con1 ="PAKISTAN "+"ZINDABAD";
+//alert('"'+con1+'"')
+
+//answer5 
+var con2 = "core";
+var con01 = 2; 
+console.log(con2+con01);
+
+// answer 6 
+var con3="hello";
+var con03="world"
+var con003= con3 + con03 ;
+console.log(con003);
+
+                                     //chapter 9
+
+
+                                     
+                                     //answer 1
+                                     var firstName = prompt("Enter first name");
+
+                                     //answer2
+                                     var country = prompt("Country?", "China");
+
+                                     // answer 3
+                                     var yourName = prompt("Enter Your Name");
+
+
+                                     //answer 4 
+                                     var info = prompt("the message ","Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus odio accusantium quibusdam harum in sit! Non voluptate, sit animi consequuntur sed quia accusamus minima tenetur saepe dolor eum expedita, incidunt recusandae. Deleniti animi nobis officiis harum consectetur magni molestiae, expedita necessitatibus, sunt, veniam odio voluptatibus sapiente nemo aspernatur temporibus quidem!") ;
+                                     
+                                     //answer 5 
+                                     var info2 = prompt("your name ","SHAH");
+                                     alert(info2);
+                                      
+                                     
+//chap 10 
+var city = "Karachi";
+if (city === "Karachi") {
+console.log("The City OF Lights")
+}
+
+//answer2
+var x = 4 ;
+var y = 4 ;
+if(x===y){
+    var z = prompt("x====y","huuuuuuuuuuey");
+} 
+
+//answer 3
+var zipcode = prompt("enter the zip code ");
+if(zipcode =="10010" ){
+    alert("karachi");
+
+}
+else{
+    alert("Please write correct city");
+}
+
+//chap 11
+
+//answer1
+var variable1 = 10; 
+var variable2 = 20; 
+
+if (variable1 !== variable2) {
+    alert("not equal ");
+} else {
+    alert("equal ");
+}
+
+
+//answer 2
+var sha01= prompt("you age" );
+
+if (sha01== 20 ) {
+    alert( "you are young" );
+    }
+
+else{
+    alert ("you are not allowed ");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
